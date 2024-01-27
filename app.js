0e6cc0905ab5c127a4f08a29a53fcdaa9ab18eb5
thuong <119164707+Thuongthph19784@users.noreply.github.com>
2024-01-28 00:49:37 +0700

    f

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -8,7 +9,7 @@ var indexRouter = require('./routes/index');
 var product = require('./routes/product');
 const mongoose = require("mongoose");
 const { required } = require('nodemon/lib/config');
-require('dotenv').config();
+
 
 // mongoose
 //   .connect("mongodb://localhost:27017/lab34", {
@@ -20,7 +21,7 @@ require('dotenv').config();
 
   require("./components/model/ProductModel");
 var app = express();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 2409
 
 mongoose.set('strictQuery',false)
 const connectDB = async ()=> {
